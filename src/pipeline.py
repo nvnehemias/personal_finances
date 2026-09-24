59630fddf6bbbb47d2f162b4f17e55832e4fa184
Nehemias Vicente <nvnehemias@gmail.com>
2026-09-24 16:19:57 -0700

    Adding to script

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,2 +1,26 @@
-from db_connection import get_connection
+import os
+import sys 
+import psycopg
+from pathlib import Path
+import logging
+from dotenv import load_dotenv
 
+def get_connection():
+
+    load_dotenv()
+    connection = None
+
+    try:
+        connection = psycopg.connect(
+            host = os.getenv("DB_HOST","127.0.0.1"),
+            dbname = os.getenv("DB_NAME"),
+            user = os.getenv("DB_USER"),
+            password = os.getenv("DB_PASSWORD"),
+            port = os.getenv("DB_PORT","5432")
+        )
+        
+        
+    except psycopg.Error as e:
+        print(f"Error connecting to PostgreSQL: {e}")
+
+    return connection
