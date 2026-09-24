a560b342dfb933fabb8ee757adc71c73d82ef9bf
Nehemias Vicente <nvnehemias@gmail.com>
2026-09-24 14:43:46 -0700

    Adding more code

--- src/pipeline.py
+++ src/pipeline.py
@@ -2,8 +2,12 @@ import os
 import requests 
 from dotenv import load_dotenv
 
-# Getting google sheet id
+# Getting google sheet information
 sheet_id = os.getenv("sheet_id")
 sheet_name = os.getenv("sheet_name")
 
 
+# Exporting google sheet
+export_url = f"https://google.com{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
+
+# Creating function to download csv
