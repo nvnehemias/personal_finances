d046b08fb9b183453eccced2d7c9b646545d9cb5
Nehemias Vicente <nvnehemias@gmail.com>
2026-09-24 17:12:39 -0700

    Adding to sql file

---
+++ sql/load_data.sql
@@ -0,0 +1,27 @@
+insert into orders (
+
+    id
+    , source
+    , amount
+    , note
+    , sub_note
+    , transaction_date
+    , date_range
+    , owner
+
+)
+
+values (
+    %s
+    ,%s 
+    ,%s 
+    ,%s 
+    ,%s 
+    ,%s
+    ,%s
+    ,%s
+)
+
+on conflict (id) do update set 
+    transaction_date = excluded.transaction_date
+;
